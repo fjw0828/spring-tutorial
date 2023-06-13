a2a71f5b574c98ad585b4b79872884ec5a24854e
fu-jw <1151242757@qq.com>
2023-06-13 22:16:36 +0800

    子模块 demo03-log -- 日志配置模块--SLF4J

--- spring-boot/demo03-log/src/test/java/com/fredo/jcl/JCLTest.java
+++ spring-boot/demo03-log/src/test/java/com/fredo/jcl/JCLTest.java
@@ -1,5 +1,7 @@
 package com.fredo.jcl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
 public class JCLTest {
@@ -7,5 +9,14 @@ public class JCLTest {
     @Test
     public void test01(){
 
+        // 1.获取日志记录器对象
+        Log log = LogFactory.getLog(JCLTest.class);
+        // 2.日志记录输出
+        log.info("Hello JCL");
+        /**
+         * 结果记录:
+         * 没有其他依赖时:
+         *  输出:15:39:25.767 [main] INFO com.fredo.jcl.JCLTest -- Hello JCL
+         */
     }
 }
