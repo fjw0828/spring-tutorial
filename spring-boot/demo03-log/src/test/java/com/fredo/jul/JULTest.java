29fbb959c5a5308baddbd8cfc995c2c09ed440d5
fu-jw <1151242757@qq.com>
2023-06-13 09:38:56 +0800

    子模块 demo03-log -- 日志配置模块--JUL自定义日志级别

--- spring-boot/demo03-log/src/test/java/com/fredo/jul/JULTest.java
+++ spring-boot/demo03-log/src/test/java/com/fredo/jul/JULTest.java
@@ -2,8 +2,8 @@ package com.fredo.jul;
 
 import org.junit.Test;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.IOException;
+import java.util.logging.*;
 
 
 public class JULTest {
@@ -32,5 +32,74 @@ public class JULTest {
         int age = 18;
 
         logger.log(Level.INFO, "用户信息:{0},{1}", new Object[]{name, age});
+    }
+
+    /**
+     * 日志级别
+     *  OFF > SEVERE > WARNING > INFO > CONFIG > FINE > FINER > FINEST > ALL
+     *  默认 INFO
+     */
+    @Test
+    public void test03(){
+        // 1.获取日志记录器对象
+        Logger logger = Logger.getLogger("com.fredo.jul.JULTest");
+
+        // 2.日志输出
+        logger.severe("severe, 输出错误信息");
+        logger.warning("warning, 警告信息");
+        logger.info("info");
+        logger.config("config");
+        logger.fine("fine");
+        logger.finer("finer");
+        logger.finest("finest");
+//        logger.log(Level.OFF,"关闭,最高级别");
+//        logger.log(Level.SEVERE,"错误,输出错误信息");
+//        logger.log(Level.WARNING,"警告,输出警告信息");
+//        logger.log(Level.INFO,"普通信息,最高级别");
+//        logger.log(Level.CONFIG,"debug信息");
+//        logger.log(Level.FINE,"关闭,最高级别");
+//        logger.log(Level.FINER,"关闭,最高级别");
+//        logger.log(Level.FINEST,"关闭,最高级别");
+//        logger.log(Level.ALL,"所有信息");
+
+    }
+
+    /**
+     * 自定义日志级别
+     */
+    @Test
+    public void test04() throws IOException {
+        // 1.获取日志记录器对象
+        Logger logger = Logger.getLogger("com.fredo.jul.JULTest");
+
+        // 关闭系统默认配置
+        logger.setUseParentHandlers(false);
+
+        // 自定义
+        // 创建ConsoleHandler
+        Handler consoleHandler = new ConsoleHandler();
+        // 创建简单格式转换对象
+        SimpleFormatter formatter = new SimpleFormatter();
+        // 关联
+        consoleHandler.setFormatter(formatter);
+        logger.addHandler(consoleHandler);
+        // 设置日志级别
+        logger.setLevel(Level.ALL);
+        consoleHandler.setLevel(Level.ALL);
+
+        // 还可以创建输出到文件的处理器
+        Handler fileHandler = new FileHandler("jul.log");
+        // 关联
+        fileHandler.setFormatter(formatter);
+        logger.addHandler(fileHandler);
+
+        // 日志输出
+        logger.severe("severe, 输出错误信息");
+        logger.warning("warning, 警告信息");
+        logger.info("info");
+        logger.config("config");
+        logger.fine("fine");
+        logger.finer("finer");
+        logger.finest("finest");
     }
 }
