d1f3776ac7c599ab4c51131b71dff0c6c58cfb37
fu-jw <1151242757@qq.com>
2023-06-17 09:37:40 +0800

    子模块 demo04-web -- web模块

--- spring-boot/demo04-web/src/main/java/com/fredo/WebMain.java
+++ spring-boot/demo04-web/src/main/java/com/fredo/WebMain.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class WebMain {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(WebMain.class, args);
     }
 }
