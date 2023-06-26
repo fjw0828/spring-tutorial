db1afd097222f334866995a5fe99a1cb6299fd57
fu-jw <1151242757@qq.com>
2023-06-26 09:43:29 +0800

    子模块 demo04-web -- web模块--4.模板引擎-text测试

--- spring-boot/demo04-web/src/main/java/com/fredo/controller/ThymeleafTestController.java
+++ spring-boot/demo04-web/src/main/java/com/fredo/controller/ThymeleafTestController.java
@@ -3,20 +3,15 @@ package com.fredo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ThymeleafTestController {
 
-    /**
-     * @PathParam: 获取请求行中的参数.servlet提供
-     * @PathVariable: 获取请求行中的参数.是Spring提供
-     * @RequestParam: 获取请求行中和请求体中的参数, 可以获取复杂参数, 文件等.是Spring提供
-     */
-    @GetMapping("hello")
-    public String hello(@RequestParam("name") String name, Model model) {
+    @GetMapping("test")
+    public String hello(Model model) {
 
-        model.addAttribute("data", name);
+        String data = "<span style='color:red'>Fredo</span>";
+        model.addAttribute("data", data);
         return "thymeleaf";
     }
 }
